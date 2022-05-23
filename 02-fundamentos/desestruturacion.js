bda8acfa5746403705861be0f7bb7d795f77f74b
Duvan Felipe Chavarro Gutiérrez <70192135+DuvanChavarro13@users.noreply.github.com>
2022-05-22 19:14:21 -0500

    subiendo los archivos desde el PC

---
+++ 02-fundamentos/desestruturacion.js
@@ -0,0 +1,27 @@
+
+const deadpool  = {
+    nombre: 'Wade',
+    apellido: 'Winston',
+    poder: 'Regeneración',
+    //edad: 50,
+    getNombre() {
+        return `${this.nombre} ${this.apellido} ${this.poder}`
+    }
+}
+
+console.log(deadpool.getNombre());
+
+//Con la desestructuracion es evitar escribir tanto codigo para extraer simples constantes de un objeto
+const { nombre, apellido, poder, edad=0} = deadpool; // Este es el objeto que tiene todas las propiedades, y entre las llaves voy a tener las propiedades que quiero extraer
+console.log(nombre, apellido, poder, edad);
+
+// OTRO USO DE LA DESDESTRUCTURACIÓN //Con Funciones 
+// Puedo desestructurar el objeto directamente en los argumentos, como en el siguiente caso
+// Se va a saber que es una desestructuración de un objeto porque está entre las llaves {}
+function imprimeHeroe( { nombre, apellido, poder, edad=0 } ){
+    //Aquí habrá una diferencia si le redefino una variable en la función, que sería una variable de función
+    nombre = 'Fernando';
+    console.log(nombre, apellido, poder, edad);
+}
+imprimeHeroe( deadpool );
+
