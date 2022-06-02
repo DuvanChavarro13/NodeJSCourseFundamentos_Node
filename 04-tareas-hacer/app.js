52cb154398208c65e98cfb132748b70b6510f9eb
DuvanChavarro13 <duvanchulupo@gmail.com>
2022-06-02 17:43:00 -0500

    Inicio de proyecto tareas por hacer

---
+++ 04-tareas-hacer/app.js
@@ -0,0 +1,13 @@
+require('colors');
+const { mostrarMenu } = require('./helpers/mensajes');
+
+
+console.clear();
+
+const main = async() => {
+     
+    console.log('Holaaa');
+    mostrarMenu();
+}
+
+main(); 
