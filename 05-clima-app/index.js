430c7e88958ff8781be3b18f1fff5bb4fa485f36
DuvanChavarro13 <duvanchulupo@gmail.com>
2022-07-26 08:44:11 -0500

    Borrando el proyecto clima app

--- 05-clima-app/index.js
+++
@@ -1,9 +0,0 @@
-const { leerInput } = require('./helpers/inquirer');
-
-
-const main = async() => {
-    const texto = await leerInput( '¿Qué desea hacer? ' );
-    console.log( texto );
-}
-
-main();
